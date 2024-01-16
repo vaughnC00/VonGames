5507dd340c34b44839f8f5079df6253ee4023ddf
vaughnC00 <156791091+vaughnC00@users.noreply.github.com>
2024-01-16 19:11:32 +0800

    Add files via upload

---
+++ BackgroundScrolling.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour
+{
+    public float velocidade;
+
+    private Vector3 _posicaoInicial;
+
+    public float _posicaoXFinal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocidade=Math.Abs(velocidade);
+        _posicaoInicial = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(-Fase.Instance.AjustaVelocidade(velocidade)* Time.deltaTime *
+                            new Vector3(1, 0, 0));
+        if (transform.position.x < _posicaoXFinal)
+        {
+            transform.position = _posicaoInicial;
+        }
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,180 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public string botaoPulo;
+    public string botaoDesliza;
+    public float forcaPulo;
+    public Transform verificaChaoTransform;
+    public float raioDoVerificador;
+    public LayerMask layerMaskChao;
+    public AudioClip soundJump;
+    public AudioClip soundSlide;
+    public float tempoMaximoDeslizando;
+    public Vector2 offSetDeslizando;
+    public Vector2 sizeDeslizando;
+
+    private static readonly int Pulo = Animator.StringToHash("pulo");
+    private static readonly int Desliza = Animator.StringToHash("desliza");
+    private Animator _animator;
+    private Rigidbody2D _rigidbody2D;
+    private AudioSource _audioSource;
+    private BoxCollider2D _boxCollider2D;
+    private float _contadorTempo;
+    private Vector2 _offSetInicial;
+    private Vector2 _sizeInicial;
+    public bool cliquePulo { get; set; }
+    public bool cliqueDesliza { get; set; }
+
+    private bool _estaDeslizando;
+
+
+    // public bool TouchParaCima()
+    // {
+    //     if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+    //         _startTouchPositionUp = Input.GetTouch(0).position;
+    //     else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+    //     {
+    //         _endTouchPositionUp = Input.GetTouch(0).position;
+    //         return _endTouchPositionUp.y > _startTouchPositionUp.y;
+    //     }
+    //     else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+    //     {
+    //         _startTouchPositionUp = Vector2.zero;
+    //         _endTouchPositionUp = Vector2.zero;
+    //     }
+    //
+    //     return false;
+    // }
+    //
+    // public bool TouchParaBaixo()
+    // {
+    //     if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+    //         _startTouchPositionDown = Input.GetTouch(0).position;
+    //     else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+    //     {
+    //         _endTouchPositionDown = Input.GetTouch(0).position;
+    //         return _endTouchPositionDown.y < _startTouchPositionDown.y;
+    //     }else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+    //     {
+    //         _startTouchPositionDown = Vector2.zero;
+    //         _endTouchPositionDown = Vector2.zero;
+    //     }
+    //
+    //     return false;
+    // }
+
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _audioSource = GetComponent<AudioSource>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+        _sizeInicial = _boxCollider2D.size;
+        _offSetInicial = _boxCollider2D.offset;
+    }
+
+    private void FixedUpdate()
+    {
+        Pular();
+        Deslizar();
+        Animation();
+    }
+
+    /// <summary>
+    /// Controla as animações do jogador
+    /// </summary>
+    private void Animation()
+    {
+        _animator.SetBool(Pulo, !EstaNoChao());
+        _animator.SetBool(Desliza, _estaDeslizando);
+    }
+
+    /// <summary>
+    /// Responsável por fazer o Player pular, O jogador só poderá pular se estiver no chão
+    /// </summary>
+    public void Pular()
+    {
+        if (!PodePular()) return;
+        cliquePulo = false;
+        _rigidbody2D.AddForce(transform.up * forcaPulo);
+        _audioSource.PlayOneShot(soundJump);
+        _contadorTempo = 0;
+        _boxCollider2D.offset = _offSetInicial;
+        _boxCollider2D.size = _sizeInicial;
+        _estaDeslizando = false;
+    }
+
+    /// <summary>
+    /// Responsável por fazer o Player deslizar, O jogador só poderá deslizar se estiver no chão
+    /// </summary>
+    public void Deslizar()
+    {
+        if (_estaDeslizando)
+        {
+            _contadorTempo += Time.deltaTime;
+            if (_contadorTempo >= tempoMaximoDeslizando)
+            {
+                _contadorTempo = 0;
+                _boxCollider2D.offset = _offSetInicial;
+                _boxCollider2D.size = _sizeInicial;
+                _estaDeslizando = false;
+            }
+        }
+
+        if (!PodeDeslizar()) return;
+        cliqueDesliza = false;
+        _rigidbody2D.AddForce(transform.up * -forcaPulo);
+        _boxCollider2D.offset = offSetDeslizando;
+        _boxCollider2D.size = sizeDeslizando;
+        _audioSource.PlayOneShot(soundSlide);
+        _estaDeslizando = true;
+    }
+
+    /// <summary>
+    /// Verifica se o Player pode deslizar
+    /// </summary>
+    /// <returns></returns>
+    public bool PodeDeslizar()
+    {
+        return (Input.GetKey(botaoDesliza) || cliqueDesliza) && !_estaDeslizando;
+    }
+
+    /// <summary>
+    /// Verifica se o Player tem condições para pular
+    /// </summary>
+    /// <returns></returns>
+    public bool PodePular()
+    {
+        return EstaNoChao() && (Input.GetKey(botaoPulo) || cliquePulo);
+    }
+
+    /// <summary>
+    /// Verifica se o jogador está tocando no chão
+    /// </summary>
+    /// <returns>true se está no chão e false caso contrário</returns>
+    public bool EstaNoChao()
+    {
+        return Physics2D.OverlapCircle(verificaChaoTransform.position, raioDoVerificador, layerMaskChao);
+    }
+
+    /// <summary>
+    /// Desenha Gizmos Para a Unity
+    /// </summary>
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(verificaChaoTransform.position, raioDoVerificador);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (collision2D.collider.CompareTag(TagConstantes.INIMIGO))
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+}
